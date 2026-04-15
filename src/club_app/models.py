695026a9ef6b1082c643d2916a143f2bdb63b87e
a7madmostafa <a.m_eg@yahoo.com>
2026-04-15 23:15:26 +0200

    Add is_vip property to Member model to determine if a member is a VIP based on their balance

--- src/club_app/models.py
+++ src/club_app/models.py
@@ -22,10 +22,14 @@ class Member(BaseModel):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    balance = models.FloatField()
 
     def __str__(self):
         return self.name
+    
+    @property
+    def is_vip(self):
+        return self.balance > 1000
 
 class Trainer(BaseModel):
     name = models.CharField(max_length=100)
