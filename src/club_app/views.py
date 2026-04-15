3aac5cb2fde617de5d7d34afe97e32f1a828cdaf
a7madmostafa <a.m_eg@yahoo.com>
2026-04-15 21:22:32 +0200

    Add branches view and template to display list of branches

--- src/club_app/views.py
+++ src/club_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Branch
 
 # Create your views here.
+
+def branches(request):
+    context = {
+        "branches": Branch.objects.all(),
+    }
+    return render(request, "branches.html", context)
