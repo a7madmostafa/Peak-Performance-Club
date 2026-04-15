9fadf525b5f03140617bb3f3a24d6d5921ee8efe
a7madmostafa <a.m_eg@yahoo.com>
2026-04-15 21:10:22 +0200

    Add BaseModel and Branch model, and register Branch in admin panel

--- src/club_app/models.py
+++ src/club_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Branch(BaseModel):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
