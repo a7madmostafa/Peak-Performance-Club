aea297369a4048fc8a2e6eacf4bb03eff299f5c9
a7madmostafa <a.m_eg@yahoo.com>
2026-04-15 21:44:58 +0200

    Add views and templates for home page and branches

--- src/club_app/urls.py
+++ src/club_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
+    path("members/", views.members, name="members"),
+    path("trainers/", views.trainers, name="trainers"),
+    path("classes/", views.classes, name="classes"),
     path("branches/", views.branches, name="branches"),
+    path("equipments/", views.equipments, name="equipments"),
 ]
--- src/club_app/views.py
+++ src/club_app/views.py
@@ -1,10 +1,46 @@
 from django.shortcuts import render
-from .models import Branch
+from .models import Branch, Member, Trainer, GymClass, Equipment
 
 # Create your views here.
+
+def home(request):
+    context = {
+        "branches": Branch.objects.all(),
+        "members": Member.objects.all(),
+        "trainers": Trainer.objects.all(),
+        "classes": GymClass.objects.all(),
+        "equipment": Equipment.objects.all(),
+    }
+    return render(request, "home.html", context)
 
 def branches(request):
     context = {
         "branches": Branch.objects.all(),
     }
     return render(request, "branches.html", context)
+
+def members(request):
+    context = {
+        "members": Member.objects.all(),
+    }
+    return render(request, "members.html", context)
+
+def trainers(request):
+    context = {
+        "trainers": Trainer.objects.all(),
+    }
+    return render(request, "trainers.html", context)
+
+def classes(request):
+    context = {
+        "classes": GymClass.objects.all(),
+        "trending_classes": GymClass.objects.trending(),
+    }
+    return render(request, "classes.html", context)
+
+def equipments(request):
+    context = {
+        "equipments": Equipment.objects.all(),
+    }
+    return render(request, "equipments.html", context)
+
